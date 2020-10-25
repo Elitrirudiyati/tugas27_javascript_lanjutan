e8fd6554bc2514231f96b3f86db44e2d542077cb
Eli Tri Rudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-25 15:07:25 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,10 @@
+
+function joinArray() {
+	var alamat1 = ["No","rumah saya",30,"A"]
+	var alamat2 = ["Jalan pangeran Tirtayasa No", 70]
+
+	console.log(alamat1.join(" "))
+	console.log(alamat2.join(" "))
+}
+
+joinArray();
